4cc53e8b2eaf535b48cdd1c0949b9e35c4d2c7ad
druther <harshit.chopra.19@gmail.com>
2015-03-18 00:19:38 +0530

    Added function for counting number bomb

---
+++ number_bomb.py
@@ -0,0 +1,25 @@
+def  contains_bomb( arr):
+    counter = 0    # Net checker to see if 3 pairs are reached 
+    count = 1      # Counting instances of each integer
+    for i in range (1,len(arr)):
+        if (arr[i] == arr[i-1]):
+            count = count+1
+        else :
+            # Condition to check if the previous integer had more than 2 instances
+            if count>=2:
+                  counter = counter+1
+                  if counter == 3:
+                    return True
+                
+            else:
+                counter = 0
+            count = 1
+            # Condition to check if the new integer and previous are consecutive
+            if arr[i]!=arr[i-1]+1:
+                counter = 0
+        
+    if count>=2:
+      counter =counter+1
+    if counter == 3:
+            return True    
+    return False    
