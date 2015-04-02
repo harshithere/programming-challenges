7545f21ffe9143b645320f575d0240421f5aedd7
druther <harshit.chopra.19@gmail.com>
2015-04-02 19:00:06 +0530

    Added program to check if given number is prime or not

---
+++ check_prime.py
@@ -0,0 +1,22 @@
+''' Program to check whether a given number is prime or not '''
+
+import math
+
+def isprime(num):
+  if num<2:
+    return False
+  elif num == 2:
+    return True
+  else:
+    for i in range(3, int(math.sqrt(num))+1 , 2):
+      if num%i==0:
+        return False
+  return True
+
+print "Enter a number to check"
+num = int(raw_input())
+result = isprime(num)
+if result == False:
+  print 'Number is not prime'
+else:
+  print 'Number is prime'
